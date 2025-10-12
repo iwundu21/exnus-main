b24bce271e76b2da8a29d713e5434acc0c1bd6f2
Oluebube James <jamesoluebube265@gmail.com>
2025-10-12 14:59:40 +0000

    Link post should be indicating as Link and be click able

--- src/components/linkify.tsx
+++ src/components/linkify.tsx
@@ -12,17 +12,18 @@ export default function Linkify({ text }: LinkifyProps) {
     return null;
   }
   
-  const urlRegex = /(https?:\/\/[^\s]+)/g;
+  const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])|(\bwww\.[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
   const parts = text.split(urlRegex);
 
   return (
     <p className="text-foreground/80 whitespace-pre-wrap">
       {parts.map((part, index) => {
-        if (part.match(urlRegex)) {
+        if (part && part.match(urlRegex)) {
+          const href = part.startsWith('www.') ? `http://${part}` : part;
           return (
             <a
               key={index}
-              href={part}
+              href={href}
               target="_blank"
               rel="noopener noreferrer"
               className="text-primary underline hover:text-primary/80"
