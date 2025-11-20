e575a6431e2041e0aebbc178322e73178aadd80a
Oluebube James <jamesoluebube265@gmail.com>
2025-11-20 16:21:56 +0000

    Now you have to correct  the distribution  of the staking rewards  vesti

--- src/components/sections/diagrams/staking-rewards-diagram.tsx
+++ src/components/sections/diagrams/staking-rewards-diagram.tsx
@@ -26,7 +26,7 @@ export function StakingRewardsDiagram() {
         <FlowStep 
           icon={<svg className="w-7 h-7 text-primary" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v.01" /></svg>}
           title="Total Rewards Pool" 
-          description="1.895 Billion Exnus Tokens allocated for staking."
+          description="1.703 Billion Exnus Tokens allocated for staking."
         />
         <Arrow />
         <FlowStep 
